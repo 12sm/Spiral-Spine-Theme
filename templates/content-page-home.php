de6c71cf6c5428b95d51318ee9fbbde9fc40d595
Michael Shoup <michael@12southmusic.com>
2014-10-07 19:06:41 -0400

    initial commit

---
+++ templates/content-page-home.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class='col-sm-12'>
+		<?php echo do_shortcode('[wpv-view name="HOME | BLOG"]'); ?>
+	</div>
+</div>
+<div class='row'>
+	<div class='col-sm-6'>
+		<p> Popular posts here</p>
+	</div>
+	<div class='col-sm-6'>
+		<?php echo do_shortcode('[wpv-view name="HOME VIDEO"]'); ?>
+	</div>
+</div>
