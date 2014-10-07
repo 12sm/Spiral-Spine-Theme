be23ddea32139607859d76c3a60893db779260a9
Ian Noyes <ian@12southmusic.com>
2014-10-07 18:58:50 -0500

    margin-top-md

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -3,7 +3,7 @@
 		<?php echo do_shortcode('[wpv-view name="HOME | BLOG"]'); ?>
 	</div>
 </div>
-<div class='row'>
+<div class='row margin-top-md'>
 	<div class='col-sm-4 col-sm-offset-2'>
 		<p> Popular posts here</p>
 	</div>
