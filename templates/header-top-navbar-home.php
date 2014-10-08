a899264b6e841f20f30a872aa613236c9efb1fb2
michael <michael@12southmusic.com>
2014-10-08 17:57:36 -0500

    Subscribe form

--- templates/header-top-navbar-home.php
+++ templates/header-top-navbar-home.php
@@ -29,7 +29,27 @@
     		<h3>Get news to learn how.</h3>
     	</div>
     	<div class="col-md-6 col-sm-12">
-    		Email Signup Here
+    	
+<div id="mc_embed_signup">
+<form action="//spiralspine.us9.list-manage.com/subscribe/post?u=d73eb5a9058233250acd4b164&amp;id=41c0f87e4f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+<div class="mc-field-group">
+	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="First Name">
+</div>	
+<div class="mc-field-group">
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="E-Mail">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_d73eb5a9058233250acd4b164_41c0f87e4f" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+
     	</div>	
     </div>	
     </div>
