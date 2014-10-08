4495882a7472ca403bc3350589eeb1da1de896ae
michael <michael@12southmusic.com>
2014-10-08 18:03:27 -0500

    Close a row

--- templates/header-top-navbar-home.php
+++ templates/header-top-navbar-home.php
@@ -32,7 +32,7 @@
     	
 <div id="mc_embed_signup">
 <form action="//spiralspine.us9.list-manage.com/subscribe/post?u=d73eb5a9058233250acd4b164&amp;id=41c0f87e4f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
+  <div id="mc_embed_signup_scroll">
     <div class="row">
 		<div class="col-md-4">
 			<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="First Name">
@@ -43,12 +43,13 @@
 		<div class="col-md-4">
 			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
 		</div>
+    </div>
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_d73eb5a9058233250acd4b164_41c0f87e4f" tabindex="-1" value=""></div>
-    </div>
+  </div>
 </form>
 </div>
 
