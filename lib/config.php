b9117ea5e25f72eef2f1d2da8728324c54deb4ab
Ian Noyes <ian@12southmusic.com>
2014-10-08 14:22:58 -0500

    like zis

--- lib/config.php
+++ lib/config.php
@@ -54,15 +54,15 @@ function roots_display_sidebar() {
      * The second element must be an array even if there's only 1 argument.
      */
     array(
-      'is_404',
-      "is_page_template('template-layouts.php')"
+      'is_404'
     ),
     /**
      * Page template checks (via is_page_template())
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
+      'template-custom.php',
+      'template-layouts.php'
     )
   );
 
