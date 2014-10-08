23745681f9d0459f8dd01c7b8acfc66728d2c046
Ian Noyes <ian@12southmusic.com>
2014-10-08 14:20:12 -0500

    no sidebar on layouts

--- lib/config.php
+++ lib/config.php
@@ -54,7 +54,8 @@ function roots_display_sidebar() {
      * The second element must be an array even if there's only 1 argument.
      */
     array(
-      'is_404'
+      'is_404',
+      "is_page_template('template-layouts.php')"
     ),
     /**
      * Page template checks (via is_page_template())
