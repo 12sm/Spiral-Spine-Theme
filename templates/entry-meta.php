42147583b8b4f09a38fc8b868a43c4cd53cea8e5
michael <michael@12southmusic.com>
2014-10-08 21:54:05 -0500

    share buttons

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,1 +1,3 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<div class="hidden-md hidden-lg"><div class="addthis_sharing_toolbox" data-url="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>"></div></div>
