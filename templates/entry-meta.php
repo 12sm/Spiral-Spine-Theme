6f916df2dab0de42f48f9ba4fff6c78882782599
michael <michael@12southmusic.com>
2014-10-08 22:49:56 -0500

    list cats?

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,3 @@
+<?php wp_list_categories( $args ); ?> 
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 <div class="addthis_sharing_toolbox" data-url="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>"></div>
