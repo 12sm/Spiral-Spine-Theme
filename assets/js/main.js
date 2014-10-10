204dc2766f2068532d5d2a7ff1513a41608806aa
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-10 12:02:39 -0500

    bounce js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,69 +16,73 @@
 
 (function($) {
 
-// Use this variable to set up the common and page specific functions. If you
-// rename this variable, you will also need to rename the namespace below.
-var Roots = {
-  // All pages
-  common: {
-    init: function() {
-      // JavaScript to be fired on all pages
-      $('.img-lq').imgLiquid();
-      $('.nav li a').hover(function () {
-        $(this).toggleClass('animated bounceIn');
+  function bounce(){
+    $(this).addClass('animated bounceIn');
+  }
+  function bounced(){
+    $(this).removeClass('animated bounceIn');
+  }
+  // Use this variable to set up the common and page specific functions. If you
+  // rename this variable, you will also need to rename the namespace below.
+  var Roots = {
+    // All pages
+    common: {
+      init: function() {
+        // JavaScript to be fired on all pages
+        $('.img-lq').imgLiquid();
+        $('.nav li').hover(bounce, bounced);
+        $(".content").fitVids();
+      }
+    },
+    // Home page
+    home: {
+      init: function() {
+        // JavaScript to be fired on the home page
+
+      }
+    },
+      // Video Archive
+    post_type_archive_video: {
+      init: function() {
+        // JavaScript to be fired on the home page
+       $('.menu-blog').removeClass('active');
+      }
+    },
+      // Video Single
+    single_video: {
+      init: function() {
+        // JavaScript to be fired on the home page
+       $('.menu-blog').removeClass('active');
+       $('.menu-video').addClass('active');
+      }
+    },
+    // About us page, note the change from about-us to about_us.
+    about_us: {
+      init: function() {
+        // JavaScript to be fired on the about us page
+      }
+    }
+  };
+
+  // The routing fires all common scripts, followed by the page specific scripts.
+  // Add additional events for more control over timing e.g. a finalize event
+  var UTIL = {
+    fire: function(func, funcname, args) {
+      var namespace = Roots;
+      funcname = (funcname === undefined) ? 'init' : funcname;
+      if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
+        namespace[func][funcname](args);
+      }
+    },
+    loadEvents: function() {
+      UTIL.fire('common');
+
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/),function(i,classnm) {
+        UTIL.fire(classnm);
       });
-      $(".content").fitVids();
     }
-  },
-  // Home page
-  home: {
-    init: function() {
-      // JavaScript to be fired on the home page
+  };
 
-    }
-  },
-    // Video Archive
-  post_type_archive_video: {
-    init: function() {
-      // JavaScript to be fired on the home page
-     $('.menu-blog').removeClass('active');
-    }
-  },
-    // Video Single
-  single_video: {
-    init: function() {
-      // JavaScript to be fired on the home page
-     $('.menu-blog').removeClass('active');
-     $('.menu-video').addClass('active');
-    }
-  },
-  // About us page, note the change from about-us to about_us.
-  about_us: {
-    init: function() {
-      // JavaScript to be fired on the about us page
-    }
-  }
-};
-
-// The routing fires all common scripts, followed by the page specific scripts.
-// Add additional events for more control over timing e.g. a finalize event
-var UTIL = {
-  fire: function(func, funcname, args) {
-    var namespace = Roots;
-    funcname = (funcname === undefined) ? 'init' : funcname;
-    if (func !== '' && namespace[func] && typeof namespace[func][funcname] === 'function') {
-      namespace[func][funcname](args);
-    }
-  },
-  loadEvents: function() {
-    UTIL.fire('common');
-
-    $.each(document.body.className.replace(/-/g, '_').split(/\s+/),function(i,classnm) {
-      UTIL.fire(classnm);
-    });
-  }
-};
-
-$(document).ready(UTIL.loadEvents);
+  $(document).ready(UTIL.loadEvents);
 
 })(jQuery); // Fully reference jQuery after this point.
